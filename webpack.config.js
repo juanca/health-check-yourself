ef989bd23123d81c74790508518cf6b671301865
Juan Carlos Medina <juanca.med@gmail.com>
2017-01-28 14:16:59 -0800

    npm install babel-loader babel-core babel-preset-es2015 --save

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,20 @@ module.exports = {
   entry: {
     app: './src/index.js',
   },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['babel-preset-es2015']
+        },
+      },
+    ],
+  },
+
   output: {
     filename: 'bundle.js',
   }
