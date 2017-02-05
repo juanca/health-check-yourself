72a3e7457a0d0c792fe12f0c3d8a5263f91522ad
Juan Carlos Medina <juanca.med@gmail.com>
2017-02-04 19:50:07 -0800

    Add SVGs for emotion-select
    Composing CSS!

--- src/emotion-select.jsx
+++ src/emotion-select.jsx
@@ -1,17 +1,34 @@
 import React from 'react';
 
-let counter = 0;
+const happySvg = require('./svg/happy-donger.svg');
+const neutralSvg = require('./svg/neutral-donger.svg');
+const sadSvg = require('./svg/sad-donger.svg');
+const styles = require('./emotion-select.css');
 
 export default class EmotionSelect extends React.Component {
   render() {
     return (
       <fieldset id={this.props.id}>
         <legend>{this.props.legend}</legend>
-        <label htmlFor={`${this.props.id}-happy`}>:)</label>
+        <label className={styles.label} htmlFor={`${this.props.id}-happy`}>
+          <svg className={styles['happy-donger']}>
+            <use xlinkHref={happySvg} />
+          </svg>
+        </label>
         <input type="radio" id={`${this.props.id}-happy`} name={this.props.id} />
-        <label htmlFor={`${this.props.id}-neutral`}>:|</label>
+
+        <label className={styles.label} htmlFor={`${this.props.id}-neutral`}>
+          <svg className={styles['neutral-donger']}>
+            <use xlinkHref={neutralSvg} />
+          </svg>
+        </label>
         <input type="radio" id={`${this.props.id}-neutral`} name={this.props.id} />
-        <label htmlFor={`${this.props.id}-sad`}>:(</label>
+
+        <label className={styles.label} htmlFor={`${this.props.id}-sad`}>
+          <svg className={styles['sad-donger']}>
+            <use xlinkHref={sadSvg} />
+          </svg>
+        </label>
         <input type="radio" id={`${this.props.id}-sad`} name={this.props.id} />
       </fieldset>
     );
